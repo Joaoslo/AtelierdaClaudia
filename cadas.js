fd038c9367bc584d08f8c24ce54be2fd01c8a781
piripiri <ttthiaio@gmail.com>
2025-06-01 19:19:40 -0300

    Update cadas.js

--- cadas.js
+++ cadas.js
@@ -4,9 +4,9 @@ const emailInput = document.getElementById("emailInput");
 const alertBox = document.getElementById("alert");
 
 submitButton.addEventListener("click", () => {
-
+   
     const email = emailInput.value.trim();
-
+    /* se email tiver @gmail.com aparecer alerta sucesso, se não aparece invalido */
         if (email && email.includes("@gmail.com")) {
             showAlert("Sucesso! Email enviado.", "success");
         } else {
@@ -15,14 +15,14 @@ submitButton.addEventListener("click", () => {
 
 });
 
-
+ /* coiso do alerta sla pesquisa no chatgpt nao lembro */
 function showAlert(message, type) {
     alertBox.textContent = message;
     alertBox.className = "alert"; 
     alertBox.classList.add(`alert-${type}`);
     alertBox.style.display = "block";
 
-       
+     /* pro alerta não ficar la pra sempre */
     setTimeout(() => {
         alertBox.style.display = "none";
     }, 3000); // 3 segundos (milesegundos)
