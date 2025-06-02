b7e1d392b6f92b6b8105c25725907a6d9b143f65
Joaoslo <joao2007.so@gmail.com>
2025-06-01 21:54:02 -0300

    Add files via upload

--- cadas.js
+++ cadas.js
@@ -1,29 +1,30 @@
 
-const submitButton = document.getElementById("submitButton");
-const emailInput = document.getElementById("emailInput");
+const nomecainput = document.getElementById("nomecainput")
+const emailinput = document.getElementById("emailinput");
+const submitButton = document.getElementById("criacadas");
 const alertBox = document.getElementById("alert");
 
 submitButton.addEventListener("click", () => {
-   
-    const email = emailInput.value.trim();
-    /* se email tiver @gmail.com aparecer alerta sucesso, se não aparece invalido */
+
+    const email = emailinput.value.trim();
+
         if (email && email.includes("@gmail.com")) {
-            showAlert("Sucesso! Email enviado.", "success");
+            
         } else {
             showAlert("Por favor, insira um e-mail válido.", "error");
     }
 
 });
 
- /* coiso do alerta sla pesquisa no chatgpt nao lembro */
+
 function showAlert(message, type) {
     alertBox.textContent = message;
     alertBox.className = "alert"; 
     alertBox.classList.add(`alert-${type}`);
     alertBox.style.display = "block";
 
-     /* pro alerta não ficar la pra sempre */
+       
     setTimeout(() => {
         alertBox.style.display = "none";
-    }, 3000); // 3 segundos (milesegundos)
+    }, 10000); 
 }
